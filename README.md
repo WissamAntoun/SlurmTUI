2a15dd9f42504d45b81978c06970d4a86e610b4f
ANTOUN Wissam <wissam.antoun@inria.fr>
2024-01-04 16:35:46 +0100

    testing pypi publishing

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # SlurmTUI
-Terminal UI for monitoring SLURM jobs
+[![PyPI version](https://badge.fury.io/py/slurmtui.svg)](https://badge.fury.io/py/slurmtui)
+![PyPI - Downloads](https://img.shields.io/pypi/dm/slurmtui)
+
+A simple Terminal UI for monitoring SLURM jobs.
+
+### Note: SlurmTUI requires slurm 21.08 or later for the Json output support.
+
+## Installation
+```bash
+pip install slurmtui
+```
+
+## Usage
+Just run `slurmtui` in your terminal.
+
+Environment variables:
+- `UPDATE_INTERVAL`: Refresh rate in seconds (default: 5)
+- `TAIL_LINES`: Number of lines to show in the tail (default: -1, show all)
+
+
+![jobui](./img/screenshot.png)
+
+# FAQ
+
+### How to select text in the App?
+JobUI is running a Textual app which puts your terminal in to application mode which disables clicking and dragging to select text. Most terminal emulators offer a modifier key which you can hold while you click and drag to restore the behavior you may expect from the command line. The exact modifier key depends on the terminal and platform you are running on.
+
+- iTerm Hold the OPTION key.
+- Gnome Terminal Hold the SHIFT key.
+- Windows Terminal Hold the SHIFT key.
+
+Refer to the documentation for your terminal emulator, if it is not listed above.
+
+# Contact
+- [Wissam Antoun](https://github.com/WissamAntoun/)
--- setup.py
+++ setup.py
@@ -2,9 +2,22 @@ from setuptools import find_packages, setup
 
 requirements = ["typer[all]==0.9.0", "rich==13.4.2", "textual==0.32.0"]
 
+
+def get_long_description():
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+
+    return long_description
+
+
 setup(
     name="slurmtui",
-    version="0.0.1",
+    version="0.1.0",
+    author="Wissam Antoun",
+    author_email="wissam.antoun@gmail.com",
+    description="A simple Terminal UI (TUI) for Slurm",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     packages=find_packages("src"),
     package_dir={"": "src"},
     entry_points={
@@ -18,4 +31,19 @@ setup(
     package_data={
         "slurmtui": ["css/*"],
     },
+    python_requires=">=3.6.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: High Performance Computing (HPC)",
+    ],
 )
