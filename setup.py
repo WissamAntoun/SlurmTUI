bc6ead760eda0979edc2be51e972fe02ec468990
Wissam Antoun <wissam.antoun@gmail.com>
2024-01-11 15:46:01 +0100

    up version to 0.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_long_description():
 
 setup(
     name="slurmtui",
-    version="0.1.2",
+    version="0.1.3",
     author="Wissam Antoun",
     author_email="wissam.antoun@gmail.com",
     description="A simple Terminal UI (TUI) for Slurm",
