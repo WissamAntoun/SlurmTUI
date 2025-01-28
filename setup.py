f47845dd0d26b7230cf3467a05c0195d6a249836
ANTOUN Wissam <wissam.antoun@inria.fr>
2025-01-28 14:30:01 +0100

    Bump version to 0.2.4 in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_long_description():
 
 setup(
     name="slurmtui",
-    version="0.2.3",
+    version="0.2.4",
     author="Wissam Antoun",
     author_email="wissam.antoun@gmail.com",
     description="A simple Terminal UI (TUI) for Slurm",
